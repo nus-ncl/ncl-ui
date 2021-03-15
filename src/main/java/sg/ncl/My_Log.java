46a98f09870ef4b329bcf638989e678d95250559
hkwany <380868513@qq.com>
2021-03-16 02:12:38 +0800

    additional log to sign2

--- src/main/java/sg/ncl/My_Log.java
+++ src/main/java/sg/ncl/My_Log.java
@@ -2,16 +2,17 @@
 package sg.ncl;
 
 public class My_Log {
-    public String GetRemoteClientIP(HttpServletRequest request){
-        String ip=request.getHeader("x-forwarded-for");
-        if(ip==null||ip.length()==0||"unknown".equalsIgnoreCase(ip)){
-            ip=request.getHeader("Proxy-Client-IP");
+    public String GetRemoteClientIP(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
         }
-        if(ip==null||ip.length()==0||"unknown".equalsIgnoreCase(ip)){
-            ip=request.getHeader("WL-Proxy-Client-IP");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
         }
-        if(ip==null||ip.length()==0||"unknown".equalsIgnoreCase(ip)){
-            ip=request.getRemoteAddr();
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
         }
         return ip;
     }
+}
